02616d888a78f2ca0781176264ea485800984190
rickyfili10 <riccardofilieri@protonmail.com>
2024-10-22 23:08:42 +0200

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -2,7 +2,7 @@ from lib.mojstd.py import *
 from bootCheck import BootCheck
 import os
 
-BootCheck()
+BootCheck() # Return True, so in case of plugin to start at boot it boot it
   
 returner()
 os.system("sudo python menu.py")
