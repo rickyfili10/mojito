6fc905896867f9ab487b443f204b013ff6e4d9c6
rickyfili10 <riccardofilieri@protonmail.com>
2024-11-07 21:04:57 +0100

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -2,8 +2,14 @@ from libs.mojstd import *
 from libs.bootCheck import *
 import os
 
-BootCheck() # Return True, so in case of plugin to start at boot it boot it 
-execPlugins() 
-  
+def mon0():
+        os.system("sudo iw wlan0 interface add mon0 type monitor")
+        os.system("sudo airmon-ng start mon0")
+
+
+mon0()
+BootCheck() # Return True, so in case of plugin to start at boot it boot it
+# execPlugins()
+
 returner()
 os.system("sudo python menu.py")
