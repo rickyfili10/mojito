fb9f80e2f8e9982803983c9dcb719d6426c29614
rickyfili10 <riccardofilieri@protonmail.com>
2024-11-18 15:44:42 +0100

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -1,10 +1,17 @@
 from libs.mojstd import *
 from libs.bootCheck import *
+from libs.updater import *
 import os
 
 def mon0():
         os.system("sudo iw wlan0 interface add mon0 type monitor")
         os.system("sudo airmon-ng start mon0")
+
+if randomCheck():
+    show_message("Checking for\n   Updates...", 1)
+    updateMain()
+else: 
+    pass
 
 
 mon0()
